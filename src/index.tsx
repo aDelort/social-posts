c6a4b1f663c9afb4d6bfb1d1dfef5a656a2060a2
Arthur DELORT <arthurdelort@hotmail.fr>
2021-11-09 19:29:54 +0100

    Create a posts/ folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { StreamedPostsChart } from './StreamedPostsChart'
+import { StreamedPostsChart } from './posts/StreamedPostsChart'
 
 ReactDOM.render(
   <React.StrictMode>
